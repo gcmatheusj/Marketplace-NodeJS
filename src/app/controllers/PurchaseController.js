0faa22b495e64b7a7510b35804fcf878cf3cc396
gcmatheusj <goca1377@gmail.com>
2019-02-26 17:23:30 -0300

    add queue com redis e jobs para purchaseMail

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -1,20 +1,20 @@
 const Ad = require('../models/Ad')
-const USer = require('../models/User')
-const Mail = require('../services/Mail')
+const User = require('../models/User')
+const PurchaseMail = require('../jobs/PurchaseMail')
+const Queue = require('../services/Queue')
 
 class PurchaseController {
   async store (req, res) {
     const { ad, content } = req.body
 
     const purchaseAd = await Ad.findById(ad).populate('author')
-    const user = await USer.findById(req.userId)
+    const user = await User.findById(req.userId)
 
-    await Mail.sendMail({
-      from: '"Matheus Castro" <matheus@email.com>',
-      to: purchaseAd.author.email,
-      subject: `Solicitação de compra: ${purchaseAd.title}`,
-      html: `<p>Teste: ${content}</p>`
-    })
+    Queue.create(PurchaseMail.key, {
+      ad: purchaseAd,
+      user,
+      content
+    }).save()
 
     return res.send()
   }
